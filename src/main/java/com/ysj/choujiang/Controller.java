c3d7e63b7eb2ee92954b95448475d28cc6633c32
yangqiaocs <ccnuysj@qq.com>
2022-01-13 00:18:58 +0800

    choujiang add choujiang

--- src/main/java/com/ysj/choujiang/Controller.java
+++ src/main/java/com/ysj/choujiang/Controller.java
@@ -1,11 +1,11 @@
 package com.ysj.choujiang;
 
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @RestController
 @RequestMapping("/api/v0/choujiang")
@@ -14,16 +14,41 @@ public class Controller {
     @Autowired
     private RedisTemplate redisTemplate;
 
-    private List<JiangPing> jiangPingList = new ArrayList<JiangPing>();
+    private List<UUID> userList = new ArrayList<UUID>();
+    private HashMap<String,Integer> jiangPingMap = new HashMap<String, Integer>();
+
 
     @GetMapping("/jiangpings")
-    public List<JiangPing> jiangPingList(){
-        return (List<JiangPing>) redisTemplate.opsForValue().get("jiangpinglist");
+    public HashMap<String,Integer> jiangPingList(){
+        return (HashMap<String,Integer>) redisTemplate.opsForValue().get("jiangpingmap");
     }
 
     @PostMapping("/jiangpings")
     public void jiangPingStore(@RequestBody JiangPing jiangPing){
-        jiangPingList.add(jiangPing);
-        redisTemplate.opsForValue().set("jiangpinglist",jiangPingList);
+        jiangPingMap.put(jiangPing.name,jiangPing.restNum);
+        redisTemplate.opsForValue().set("jiangpingmap",jiangPingMap);
     }
+
+    @PostMapping("")
+    public Integer chouJiang(){
+        Integer num = new Random().nextInt(20);
+        if(jiangPingMap.get("iphone 13")<=0){
+            return -1;
+        }
+        if(num<10){
+
+            synchronized (jiangPingMap){
+                return jiangPingMap.compute("iphone 13",(key,value) -> {
+                    if (value == 0){
+                        userList.add(UUID.randomUUID());
+                        return 0;
+                    }else{
+                        return value-1;
+                    }
+                });
+            }
+        }
+        return -1;
+    }
+
 }
